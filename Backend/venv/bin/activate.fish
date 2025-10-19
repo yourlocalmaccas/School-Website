d9661196051ab2988b08f00fafe7169259576824
Marcus Papasavvas <codingacc30@gmail.com>
2025-10-20 10:12:19 +1100

    v9 fixed pip instaliation and venv

--- Backend/venv/bin/activate.fish
+++ Backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/home/marcus/Desktop/School Website/Backend/venv'
+set -gx VIRTUAL_ENV /home/marcus/Desktop/Website/Backend/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
